81ad081580c1d1c862e592eb0c50305964f07edc
Dalton Barreto <daltonmatos@gmail.com>
2020-02-26 17:36:27 -0300

    Deixando CardService.save_compra() como placeholder

--- tests/test_service.py
+++ tests/test_service.py
@@ -18,14 +18,5 @@ class ServiceTest(TestCase):
 
         self.assertEqual([card, card_2], CardService.list_cards())
 
-    async def test_save_compra(self):
-        compra = Compra(data="2020-02-26", valor=100, card_id="10")
-        card = Card(numero="10", acc_id="1234")
-
-        await CardService.save_card(card.acc_id, card)
-        CardService.save_compra("10", compra)
-
-        self.assertEqual([compra], CardService.list_compras_by_card_id("10"))
-
     async def test_list_compra_por_card_id_empty_list(self):
         self.assertEqual([], CardService.list_compras_by_card_id("10"))
